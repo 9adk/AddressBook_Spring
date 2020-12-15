7dbba5ad10cd47b21bd65cc01894b79e12b27bf5
aditya Kharade <adityakharade51@gmail.com>
2020-12-15 23:02:13 +0530

    Added: used lombok library for logginf

--- src/main/java/com/capgemini/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/capgemini/addressbookapp/AddressBookAppApplication.java
@@ -3,11 +3,13 @@ package com.capgemini.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AddressBookAppApplication {
+import lombok.extern.slf4j.Slf4j;
 
+@SpringBootApplication
+@Slf4j
+public class AddressBookAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("AddressBook app Started");
 	}
-
 }
